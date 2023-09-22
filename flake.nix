fcf8214a4a310c3a3b70b42884c9807a14a7a0e5
Svenum <s.ziegler@holypenguin.net>
2023-09-23 00:12:41 +0200

    Update nixos-version

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Solaar is Open Source Logitech Driver for Linux";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
   };
 
   outputs = { self, nixpkgs }: {
